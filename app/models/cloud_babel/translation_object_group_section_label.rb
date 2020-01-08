bf68c36aa9c5af58ea00eba38a19bf68bf4c84a9
Luis Donis <ldonis.emc2@gmail.com>
2020-01-08 10:17:54 -0600

    add clean label string method before save

--- app/models/cloud_babel/translation_object_group_section_label.rb
+++ app/models/cloud_babel/translation_object_group_section_label.rb
@@ -2,5 +2,16 @@ module CloudBabel
     class TranslationObjectGroupSectionLabel < ApplicationRecord
         belongs_to :section, class_name: 'TranslationObjectGroupSection', foreign_key: 'cloud_babel_translation_object_group_sections_id'
         belongs_to :user, class_name: '::User', foreign_key: 'users_id'
+
+        before_save :clean_label_string
+
+        def clean_label_string
+            self.label = self.label
+                .downcase                       # string to lowercase
+                .gsub(/[^0-9A-Za-z\s\-]/, '')   # remove special characters from string
+                .gsub(/-/, '_')                 # replace dashes with underscore
+                .gsub(/\s+/, '_')               # replace spaces or spaces with single dash
+        end
+
     end
 end
