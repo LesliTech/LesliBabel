c614fd8e6c65aee50aa0cdbed76517371a3c41b5
Luis Donis <ldonis.emc2@gmail.com>
2020-01-16 10:48:52 -0600

    remove unnecessary test folder

--- test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount CloudBabel::Engine => "/cloud_babel"
-end
--- test/models/cloud_babel/translation_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module CloudBabel
-  class TranslationTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
