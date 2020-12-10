9485fe0f27b3ca318b4e9bbcb0c6139e30905e6f
Carlos Hermosilla <efrainh12@galileo.edu>
2020-12-10 08:11:45 -0600

    add trello/1807 scaffold templates directory changed

--- lib/cloud_babel/engine.rb
+++ lib/cloud_babel/engine.rb
@@ -1,40 +1,36 @@
 =begin
+Copyright (c) 2020, all rights reserved.
 
-Lesli
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
 
-Copyright (c) 2020, Lesli Technologies, S. A.
-
-All the information provided by this website is protected by laws of Guatemala related
-to industrial property, intellectual property, copyright and relative international laws.
-Lesli Technologies, S. A. is the exclusive owner of all intellectual or industrial property
-rights of the code, texts, trade mark, design, pictures and any other information.
-Without the written permission of Lesli Technologies, S. A., any replication, modification,
+Without the written permission of the owner, any replication, modification,
 transmission, publication is strictly forbidden.
+
 For more information read the license file including with this software.
 
-LesliCloud - Your Smart Business Assistant
-
-Powered by https://www.lesli.tech
-Building a better future, one line of code at a time.
-
-@license  Propietary - all rights reserved.
-@version  0.1.0-alpha
-
 // · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
-// ·
+// · 
 
 =end
-
 module CloudBabel
     class Engine < ::Rails::Engine
         isolate_namespace CloudBabel
 
+        # register engine migrations path
         initializer :append_migrations do |app|
             unless app.root.to_s.match root.to_s
                 config.paths["db/migrate"].expanded.each do |expanded_path|
                     app.config.paths["db/migrate"] << expanded_path
                 end
             end
+        end
+
+        # register templates path in the core
+        config.generators do |g|
+            g.templates.unshift File::expand_path('../../../../../lib/templates', __FILE__)
         end
     end
 end
